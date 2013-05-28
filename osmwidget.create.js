fc91a3f011e95172ad90470c6fa62acc14bd73cd
spion <gorgi.kosev@gmail.com>
2013-05-29 00:31:09 +0200

    avoid minicolors invisibility

--- osmwidget.create.js
+++ osmwidget.create.js
@@ -221,7 +221,7 @@ $(document).ready(function () {
                     $("#editPoly .fill").val(style.fillColor);
                     $("#editPoly .cbFill").val(style.fill);
 
-                    pickers.minicolors({letterCase: 'uppercase'});
+                    pickers.minicolors({letterCase: 'uppercase', inline:true});
                     $("#editPoly").dialog('option', 'buttons', {
                         Save:function() {
                             style.color     = $("#editPoly .stroke").val();
